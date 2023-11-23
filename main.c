17fa13bf7fccf959b0c8d6492a48517274e6a429
Caue Sordi Paulino <132071046+caueSordi@users.noreply.github.com>
2023-11-23 00:40:01 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,20 +1,24 @@
-#include <stdio.h>
+No-Enthusiasm-2881#include <stdio.h>
 #include <stdlib.h>
-
+#include "arvore.h"
 int main()
 {
   char*filename = "arq.txt";
   FILE *fp = fopen(filename, "r");
 
+ cria_ArvBin();
+
+
   if(fp == NULL);
   {
       printf("o banco de dados %s foi sabotado, gloria a revolucao", filename);
       return 1;
-  }
+  };
   char letr;
     while ((letr = fgetc(fp)) != "x")
         putchar(letr);
  // aqui eu preciso adicionar o char coletado aos tads
+ insere_ArvBin(ArvBin, letr);
     // close the file
     fclose(fp);
 
