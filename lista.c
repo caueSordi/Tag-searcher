f8c49735b5c191376de10e4c561466051f5ddacc
Caue Sordi Paulino <132071046+caueSordi@users.noreply.github.com>
2023-12-04 13:41:54 -0300

    lista.c

---
+++ lista.c
@@ -0,0 +1,27 @@
+#include "lista.h"
+#define tamanho_tag 9
+
+// strct
+
+typedef struct inf
+{
+    ins status;
+    char tag[tamanho_tag];
+} info;
+
+typedef struct _doubly_node
+{
+    TypeData val;
+    struct _doubly_node *ant;
+    struct _doubly_node *prox;
+
+} Node;
+
+typedef struct _doubly_List
+{
+    Node *começo;
+    Node *fim;
+    Node *cursor;
+    int tamanho;
+
+} List;
