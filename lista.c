3040003524dd524b2f887a2155ed360ae0ae4782
Caue Sordi Paulino <132071046+caueSordi@users.noreply.github.com>
2023-12-09 17:09:30 -0300

    Update lista.c

--- lista.c
+++ lista.c
@@ -9,21 +9,44 @@ typedef struct inf
     char tag[tamanho_tag];
 } info;
 
-typedef struct _doubly_node
+typedef struct no_duplo
 {
     TypeData val;
-    struct _doubly_node *ant;
-    struct _doubly_node *prox;
+    struct no_duplo *ant;
+    struct no_duplo *prox;
 
 } Node;
 
-typedef struct _doubly_List
+typedef struct no_duplo
 {
-    Node *começo;
+    Node *inicio;
     Node *fim;
     Node *cursor;
     int tamanho;
 
 } List;
 //funcoes
+
+Node *Cria_no(TypeData val)
+{
+    Node*node =(Node*)calloc(1,sizeof(Node));
+strncpy(node -> val.tag,val.tag,tamanho_tag);
+node -> val.tag[strlen(node -> val.tag)] = '\0';
+node ->  val.status = val.status;
+
+return node;
+}
+
+
+Lista *Cria_lista()
+{
+    Lista *L = (Lista*)calloc(1 , sizeof(Lista));
+
+L -> cursor = NULL;
+L ->inicio = NULL;
+L -> tamanho = NULL;
+L -> fim = NULL;
+
+return L;
+}
 
