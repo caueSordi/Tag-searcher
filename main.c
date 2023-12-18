7e8470a5d2bb0f712fed2d6d1f108965a48aec02
Caue Sordi Paulino <132071046+caueSordi@users.noreply.github.com>
2023-12-18 15:43:35 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h> // Add the missing header file for string functions
 #include "arvore.h"
 #include "lista.h"
 
@@ -8,91 +8,90 @@
 
 int main()
 {
-    char line[t_tamanho];
+  char line[t_tamanho];
 
-     List *L= List_Create();
-    ArvBin* root = cria_ArvBin();
-    FILE *fp = fopen("arq.txt", "r");
+  List *L = List_Create();
 
-    if(fp == NULL)
+  ArvBin* root = cria_ArvBin();
+
+  FILE *fp = fopen("arq.txt", "r");
+
+  if (fp == NULL)
+  {
+    printf("erro");
+  }
+  else
+  {
+    char c = 'x';
+
+    fgets(line, t_tamanho, fp);
+
+    line[strcspn(line, "\r\n")] = 0;
+
+    line[strlen(line)] = '\0';
+
+    while (strncmp(line, &c, 1) != 0)
     {
-        printf("erro");
-    }
-    else
-    {
-        char c = 'x';
-
-        fgets(line, tSize, fp);
-
-        line[strcspn(line, "\r\n")] = 0;
-
-        line[strlen(line)] = '\0';
+      Info nova_info;
+      strcpy(nova_info.tag, line);
+      nova_info.status = atoi(fgets(line, t_tamanho, fp));
 
 
-        while(strncmp(line, &c, 1) != 0)
-        {
-            Info nova_info;
-            strcpy(nova_info.tag, line);
 
-            nova_info.status = atoi(fgets(line, t_tamanho, fp));
-
-            List_AddInOrder(L, nova_info);
-            insere_ArvBin(root, nova_info);
-
-            fgets(line, tSize, fp);
-            line[strcspn(line, "\r\n")] = 0;
-            line[strlen(line)] = '\0';
-        }
-
-        fclose(fp);
+      fgets(line, t_tamanho, fp);
+      line[strcspn(line, "\r\n")] = 0;
+      line[strlen(line)] = '\0';
     }
 
-    char *option;
-    char *tag;
-    char command[15];
-    fflush(stdin);
+    fclose(fp);
+  }
 
-    fgets(command, 15, stdin);
-    command[strcspn(command, "\r\n")] = 0;
+  char *option;
+  char *tag;
+  char command[15];
+  fflush(stdin);
 
-    option = strtok(command, " ");
-    tag = strtok(NULL, " ");
+  fgets(command, 15, stdin);
+  command[strcspn(command, "\r\n")] = 0;
 
-    switch(atoi(option))
+  option = strtok(command, " ");
+  tag = strtok(NULL, " ");
+
+  switch (atoi(option))
+  {
+    case 0:
     {
-        case 0:
-        {
-            printf("NL:%d NA:%d\n", List_GetSize(L), totalNO_ArvBin(root));
-            break;
-        }
-        case 1:
-        {
-            int pos_lista, treePos;
-
-            pos_lista = List_Search(L, tag);
-            treePos = consulta_ArvBin(root, tag);
-
-            if((treePos > 0) && (pos_lista > 0))
-            {
-                printf("S:0 NL:%d NA:%d\n", pos_lista, treePos);
-            }
-            else
-            {
-                printf("S:-1 NL:%d NA:%d\n", -pos_lista, -treePos);
-            }
-            break;
-        }
-        case 2:
-        {
-            imprime_lista(L);
-            break;
-        }
-        case 3:
-        {
-            preOrdem_ArvBin(root);
-            break;
-        }
+      printf("NL:%d NA:%d\n", List_GetSize(L), totalNO_ArvBin(root));
+      break;
     }
+    case 1:
+    {
+      int pos_lista, treePos;
 
-    return 0;
+      pos_lista = List_Search(L, tag);
+      treePos = consulta_ArvBin(root, tag);
+
+      if ((treePos > 0) && (pos_lista > 0))
+      {
+        printf("S:0 NL:%d NA:%d\n", pos_lista, treePos);
+      }
+      else
+      {
+        printf("S:-1 NL:%d NA:%d\n", -pos_lista, -treePos);
+      }
+      break;
+    }
+    case 2:
+    {
+      imprime_lista(L);
+      break;
+    }
+    case 3:
+    {
+      preOrdem_ArvBin(root);
+      break;
+    }
+  }
+
+  return 0;
 }
