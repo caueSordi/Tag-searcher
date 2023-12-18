fd386a53670fcfba1dce9a8b2cf0d155ad403bdf
Caue Sordi Paulino <132071046+caueSordi@users.noreply.github.com>
2023-12-18 14:38:04 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,26 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "arvore.h"
+#include "lista.h"
+
+#define t_tamanho 20
+
 int main()
 {
-  char*filename = "arq.txt";
-  FILE *fp = fopen(filename, "r");
+    char line[t_tamanho];
 
- cria_ArvBin();
+     List *L= List_Create();
+    ArvBin* root = cria_ArvBin();
+    FILE *fp = fopen("arq.txt", "r");
+
+    if(fp == NULL)
+    {
+        printf("erro");
+    }
+    else
+    {
+        char c = 'x';
+
+        fgets(line, tSize, fp);
+
+        line[strcspn(line, "\r\n")] = 0;
+
+        line[strlen(line)] = '\0';
 
 
-  if(fp == NULL);
-  {
-      printf("o banco de dados %s foi sabotado, gloria a revolucao", filename);
-      return 1;
-  };
-  char letr;
-    while ((letr = fgetc(fp)) != "x")
-        putchar(letr);
- // aqui eu preciso adicionar o char coletado aos tads
- insere_ArvBin(ArvBin, letr);
-    // close the file
-    fclose(fp);
+        while(strncmp(line, &c, 1) != 0)
+        {
+            Info nova_info;
+            strcpy(nova_info.tag, line);
+
+            nova_info.status = atoi(fgets(line, t_tamanho, fp));
+
+            List_AddInOrder(L, nova_info);
+            insere_ArvBin(root, nova_info);
+
+            fgets(line, tSize, fp);
+            line[strcspn(line, "\r\n")] = 0;
+            line[strlen(line)] = '\0';
+        }
+
+        fclose(fp);
+    }
+
+    char *option;
+    char *tag;
+    char command[15];
+    fflush(stdin);
+
+    fgets(command, 15, stdin);
+    command[strcspn(command, "\r\n")] = 0;
+
+    option = strtok(command, " ");
+    tag = strtok(NULL, " ");
+
+    switch(atoi(option))
+    {
+        case 0:
+        {
+            printf("NL:%d NA:%d\n", List_GetSize(L), totalNO_ArvBin(root));
+            break;
+        }
+        case 1:
+        {
+            int pos_lista, treePos;
+
+            pos_lista = List_Search(L, tag);
+            treePos = consulta_ArvBin(root, tag);
+
+            if((treePos > 0) && (pos_lista > 0))
+            {
+                printf("S:0 NL:%d NA:%d\n", pos_lista, treePos);
+            }
+            else
+            {
+                printf("S:-1 NL:%d NA:%d\n", -pos_lista, -treePos);
+            }
+            break;
+        }
+        case 2:
+        {
+            imprime_lista(L);
+            break;
+        }
+        case 3:
+        {
+            preOrdem_ArvBin(root);
+            break;
+        }
+    }
 
     return 0;
 }
